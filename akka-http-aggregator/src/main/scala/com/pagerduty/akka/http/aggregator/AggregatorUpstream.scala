d08e2d48b0c6d414ba4d44b5cd465ed5c9c55193
David van Geest <dvangeest@pagerduty.com>
2018-08-10 17:15:39 -0400

    Add X-Request-Id header to upstream aggregator requests.

--- akka-http-aggregator/src/main/scala/com/pagerduty/akka/http/aggregator/AggregatorUpstream.scala
+++ akka-http-aggregator/src/main/scala/com/pagerduty/akka/http/aggregator/AggregatorUpstream.scala
@@ -3,6 +3,7 @@ package com.pagerduty.akka.http.aggregator
 import akka.http.scaladsl.model.HttpRequest
 import com.pagerduty.akka.http.headerauthentication.model.HeaderAuthConfig
 import com.pagerduty.akka.http.proxy.Upstream
+import com.pagerduty.akka.http.support.RequestIdHeader
 
 trait AggregatorUpstream[AddressingConfig] extends Upstream[AddressingConfig] {
   def prepareAggregatorRequestForDelivery(
@@ -10,10 +11,16 @@ trait AggregatorUpstream[AddressingConfig] extends Upstream[AddressingConfig] {
       request: HttpRequest,
       modelRequest: HttpRequest
   ): HttpRequest = {
+    val reqWithMaybeIdHeader = modelRequest.header[RequestIdHeader] match {
+      case Some(h) => request.addHeader(h)
+      case None => request
+    }
+
     // we will always have an auth header by this point
     val authHeader = modelRequest.headers
       .find(_.is(authConfig.authHeaderName.toLowerCase))
       .get
-    request.addHeader(authHeader)
+
+    reqWithMaybeIdHeader.addHeader(authHeader)
   }
 }
