e1d8bd6e31620ef0293dee627e9161cd3bee1ad6
David van Geest <dvangeest@pagerduty.com>
2018-09-20 12:55:51 -0400

    Clean up variables.

--- akka-http-proxy/src/main/scala/com/pagerduty/akka/http/proxy/HttpProxy.scala
+++ akka-http-proxy/src/main/scala/com/pagerduty/akka/http/proxy/HttpProxy.scala
@@ -22,13 +22,12 @@ class HttpProxy[AddressingConfig](
 
   def request(request: HttpRequest,
               upstream: Upstream[AddressingConfig]): Future[HttpResponse] = {
-    val targetedRequest =
-      upstream.addressRequestWithOverrides(request, addressingConfig)
+    val addressedRequest =
+      upstream
+        .addressRequestWithOverrides(request, addressingConfig)
+        .removeHeader(TimeoutAccessHeaderName) // Akka HTTP server adds the Timeout-Access for internal reasons, but it should not be proxied
 
-    val proxyRequest =
-      targetedRequest.removeHeader(TimeoutAccessHeaderName) // Akka HTTP server adds the Timeout-Access for internal reasons, but it should not be proxied
-
-    val preparedProxyRequest = upstream.prepareRequestForDelivery(proxyRequest)
+    val preparedProxyRequest = upstream.prepareRequestForDelivery(addressedRequest)
 
     val stopwatch = Stopwatch.start()
     val response = httpClient(preparedProxyRequest)
