e0ce96b3fa6151ef81b1cc62cf13488757fc1620
David van Geest <dvangeest@pagerduty.com>
2018-11-01 16:58:38 -0400

    Remove unused imports.

--- arrivals-api/src/main/scala/com/pagerduty/arrivals/api/filter/RequestFilter.scala
+++ arrivals-api/src/main/scala/com/pagerduty/arrivals/api/filter/RequestFilter.scala
@@ -2,8 +2,6 @@ package com.pagerduty.arrivals.api.filter
 
 import akka.http.scaladsl.model.HttpRequest
 
-import scala.concurrent.Future
-
 trait RequestFilter[-RequestData] {
   def apply(request: HttpRequest, data: RequestData): RequestFilterOutput
 }
