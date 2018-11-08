044c23b3c72a15a5456205266a98f65d6a80efa0
Dimitar Roustchev <dimitar@pagerduty.com>
2018-11-08 16:13:15 -0500

    Add example for composing filters

--- arrivals-example/src/main/scala/com/pagerduty/arrivals/example/ExampleResponseFilterOne.scala
+++ arrivals-example/src/main/scala/com/pagerduty/arrivals/example/ExampleResponseFilterOne.scala
@@ -2,12 +2,13 @@ package com.pagerduty.arrivals.example
 
 import akka.http.scaladsl.model._
 import com.pagerduty.arrivals.api.filter.{SyncResponseFilter, SyncResponseFilterOutput}
+import com.pagerduty.arrivals.impl.filter.ComposableResponseFilter
 
-object ExampleResponseFilter extends SyncResponseFilter[Any] {
+object ExampleResponseFilterOne extends SyncResponseFilter[Any] with ComposableResponseFilter[Any] {
   def applySync(request: HttpRequest, response: HttpResponse, data: Any): SyncResponseFilterOutput = {
     response match {
       case resp @ HttpResponse(StatusCodes.OK, _, HttpEntity.Strict(_, body), _) =>
-        resp.withEntity(body.utf8String + "\n\nThis line was added in a response filter")
+        resp.withEntity(body.utf8String + "\n\nThis line was added in the first response filter")
       case resp @ _ => resp
     }
   }
