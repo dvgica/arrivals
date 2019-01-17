956875ab69928d08112ad704459155d48b8d7921
David van Geest <dvangeest@pagerduty.com>
2019-01-17 14:47:25 -0500

    The result of filter composition should be composable.

--- arrivals/src/main/scala/com/pagerduty/arrivals/impl/filter/ComposableResponseFilter.scala
+++ arrivals/src/main/scala/com/pagerduty/arrivals/impl/filter/ComposableResponseFilter.scala
@@ -8,8 +8,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 trait ComposableResponseFilter[-RequestData] extends api.filter.ResponseFilter[RequestData] { base =>
 
-  def ~>[T <: RequestData](filter: ResponseFilter[T])(implicit ec: ExecutionContext): ResponseFilter[T] = {
-    new ResponseFilter[T] {
+  def ~>[T <: RequestData](filter: ResponseFilter[T])(implicit ec: ExecutionContext): ComposableResponseFilter[T] = {
+    new ComposableResponseFilter[T] {
       override def apply(request: HttpRequest, response: HttpResponse, data: T): Future[HttpResponse] = {
         base.apply(request, response, data).flatMap { resp =>
           filter.apply(request, resp, data)
